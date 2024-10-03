0718f58a43d0407b64270feceb947d01d16d9635
Carolini <carolini.tsi@gmail.com>
2024-10-03 11:09:44 -0300

    Aprendendo a trabalhar com Jest.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('App', () => {
+  
+it('Deverá renderizar os elementos corretamente', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.queryByText('Hello,');
+  expect(textElement).toBeInTheDocument();
+
+  const inputElement = screen.queryByPlaceholderText('digite seu nome');
+  expect(inputElement).toBeInTheDocument();
+
+  const imageElement = screen.queryByRole('img');
+  expect(imageElement).toBeInTheDocument();
 });
+
+it('Deverá encontrar a imagem com o texto alternativo correto', () => {
+  render(<App/>);
+
+  const altTextImageElement = screen.queryByAltText('ilustração de uma mulher negra usando o computador e segurando uma xícara');
+  expect(altTextImageElement).toBeInTheDocument();
+
+})
+
+it('Deverá pegar o input corretamente', () => {
+  render(<App/>);
+
+  const inputElement = screen.queryByPlaceholderText('digite seu nome');
+  fireEvent.change(inputElement, {
+    target: {
+      value: 'Carol'
+    }
+  })
+
+  const value = screen.queryByText('Carol');
+  expect(value).toHaveTextContent('Carol');
+})
+
+it('Deverá renderizar corretamente o texto digitado', () => {
+  render(<App/>);
+
+  const inputElement = screen.queryByPlaceholderText('digite seu nome');
+  fireEvent.change(inputElement, {
+    target: {
+      value: 'Carol'
+    }
+  })
+
+  expect(screen.queryByText('Carol')).toHaveTextContent('Carol');
+  expect(screen.queryByText('Hello,')).toHaveTextContent('Hello, Carol')
+
+})
+
+})
